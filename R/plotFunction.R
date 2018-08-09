f137309ed37c3f2aaf623a274436553998ca2c36
jmzobitz <zobitz@augsburg.edu>
2018-08-08 21:28:43 -0500

    try1

---
+++ R/plotFunction.R
@@ -0,0 +1,41 @@
+### Code for plotting a simple function of x and y in R
+### Author: JMZ
+### Modified: 12/15/15
+
+### Inputs:
+### x: the independent variable
+### y: the dependent variable
+### xlabel: value for the x label axis
+### ylabel: value for the y label axis
+
+### Output: a plot to the console of the relationship, joined as a smooth line.
+### Assumes that the equation has random noise in the transmissing
+
+#' @import tidyverse
+plotFunction <- function(x,y,xLabel='x',yLabel='y') {
+
+
+
+  inputData=data.frame(x=x,y=y);
+  ### The function "melt" puts the data from a matrix format to a vector for easy grouping
+
+
+  ### Do a line plot
+  print (
+    ggplot(inputData,aes(x=x,y=y)) +
+      geom_line(size=1.0) +
+      theme(plot.title = element_text(size=20),
+            axis.title.x=element_text(size=20),
+            axis.text.x=element_text(size=15),
+            axis.text.y=element_text(size=15),
+            axis.title.y=element_text(size=20)) +
+      labs(x = xLabel,y = yLabel)
+  )
+
+
+
+
+
+
+
+}
