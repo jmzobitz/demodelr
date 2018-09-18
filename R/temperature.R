034823daa88a464b89c1aeb54051549670e4fdee
jmzobitz <zobitz@augsburg.edu>
2018-09-18 15:27:57 -0500

    includes temperature data for fitting.

---
+++ R/temperature.R
@@ -0,0 +1,16 @@
+#' Measured average global temperature by year
+#'
+#' A dataset containing phosophorous content in Daphnia and algae. The variables are as follows:
+#'
+#' \itemize{
+#'   \item yearSince1880. The year since 1880 (year)
+#'   \item globalTemp. Average global temperature (degrees Celsius)
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name temperature
+#' @usage data(temperature)
+#' @format A data frame with 135 rows and 2 variables
+#' @source The data were collected from NOAA. (https://climate.nasa.gov/vital-signs/global-temperature/)
+NULL
