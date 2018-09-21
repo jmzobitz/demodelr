eafc57ab71067b387cf035318b84311452ade704
jmzobitz <zobitz@augsburg.edu>
2018-09-20 22:28:55 -0500

    new likelihood plotting functions and documentation.

---
+++ R/plotLikelihood.R
@@ -0,0 +1,63 @@
+#' Likelihood plot of a two parameter model
+#'
+#' \code{plotLikelihood} makes a likelihood plot of a two parameter model.
+#'
+#' @param data two column of data to be plotted.
+#' First column is independent variable. Second column dependent variable.
+#' Must be a data.frame
+#' @param model a function or model of our situation
+#' @param parameters The values of the parameters we are using
+#' @param logLikely Do we compute the log likelihood function (default is FALSE)
+#' @return A plot of your likelihood function, with optimum value identified
+#' @examples
+#' # Run the vignette that works through an example
+#' vignette("likelihood")
+#'
+#' @import ggplot2
+#' @import dplyr
+#' @export
+
+
+
+plotLikelihood <-function(model,data,parameters,logLikely=FALSE) {
+
+  likelihood <- function(y,indata,logLikely=FALSE){
+    error = sd(indata-y)
+    singlelikelihoods = dnorm(indata, mean = y, sd = error, log = logLikely)
+
+    if (logLikely) {
+      return(sum(singlelikelihoods))
+    } else {
+      return(prod(singlelikelihoods))
+    }
+
+
+  }
+
+
+  lValues <- parameters %>%
+    apply(1,model, timeVector) %>%  # Apply the logistic function to each of the times (the rows of param)
+    apply(2,likelihood,data[[2]],logLikely) %>%  # Apply the likelihood function to each of the columns
+    data.frame(l_hood=.) %>%
+    cbind(parameters)
+
+
+  optValue <- lValues %>% arrange(desc(l_hood)) %>% head(n=1)
+  print(optValue)
+  lValues %>%
+    ggplot(aes(x=parameters[[1]], y=parameters[[2]], z = l_hood))+
+    geom_tile(aes(fill = l_hood)) + stat_contour()+
+    geom_point(data=optValue,aes(x=optValue[[2]],y=optValue[[3]]),size=4,color="red") +
+    labs(title="Contour Plot of Likelihood Function",
+         x=names(parameters)[1],
+         y=names(parameters)[2],
+         fill="Likelihood")
+
+
+
+
+}
+
+
+
+
---
+++ R/yeast.R
@@ -0,0 +1,16 @@
+#' Measured Sacchromyces data (yeast) from Gause 1932 "Experimental studies on the struggle for coexistence"
+#'
+#' A dataset containing phosophorous content in Daphnia and algae. The variables are as follows:
+#'
+#' \itemize{
+#'   \item time. (hours)
+#'   \item volume. Sacchromyces volume in container (cubic centimeters)
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name yeast
+#' @usage data(teast)
+#' @format A data frame with 7 rows and 2 variables
+#' @source Table 1 from Gause, G. F. 1932. “Experimental Studies on the Struggle for Existence: I. Mixed Population of Two Species of Yeast.” Journal of Experimental Biology 9 (4): 389–402.
+NULL
