35a0191bd068f8796e6dc741154ec035c7aa7be4
jmzobitz <zobitz@augsburg.edu>
2018-09-27 15:28:36 -0500

    more bootstrap stuff

---
+++ R/bootstrap_samples.R
@@ -0,0 +1,52 @@
+#'
+#' \code{bootstrap_samples} computes a given number of bootstrap samples and computes the distribution of bootstrap results.  The resulting histogram of samples is shown, along with a sampling of the results
+
+
+#' @param data one column of data to take bootstrap samples of.
+#' @param n number of bootstrap samples
+#' @param x_label value for the x label axis
+
+
+#' @source Code is adapted from: \url{https://cran.r-project.org/web/packages/broom/vignettes/bootstrapping.html}
+#'
+#' @examples
+#'
+#' # Identify the name of the data set you wish to run a regression on and labels
+#' for the axis.
+#' my_data <- data.frame(precipitation$precip)
+
+#'
+#' #' Identify the basic formula of the regression and plotting formula. Make sure you have the same names of the columns in your data.
+#' regression_formula = height ~ 1 + age
+#'
+#' bootstrap_samples(my_data,n=100,'Precipitation')
+
+#' @import ggplot2
+#' @import modelr
+#' @import broom
+#' @export
+
+
+bootstrap_samples <- function(data,n=100,x_label='x') {
+
+
+  # Generate a bootstrap estimate
+  boot_models <- modelr::bootstrap(data, n)
+
+  mean_calc <- boot_models$strap %>% as.data.frame() %>% map_df(mean,na.rm=TRUE) %>% t() %>% as.data.frame()
+  print('Mean confidence interval:')
+  print(quantile(mean_calc[[1]],probs=(c(0.025,0.5,0.975))))
+
+  sd_calc <- boot_models$strap %>% as.data.frame() %>% map_df(sd,na.rm=TRUE) %>% t() %>% as.data.frame()
+  print('Standard deviation confidence interval:')
+  print(quantile(sd_calc[[1]],probs=(c(0.025,0.5,0.975))))
+
+
+  mean_calc %>% ggplot(aes(x=V1)) + geom_histogram() + xlab(x_label) + title("Bootstrap estimate of mean")
+
+  sd_calc %>% ggplot(aes(x=V1)) + geom_histogram() + xlab(x_label) + title("Bootstrap estimate of standard deviation")
+
+
+}
+
+
---
+++ R/snowfall.R
@@ -0,0 +1,19 @@
+#' Measured snowfall from a blizzard in April 2018
+#'
+#' A dataset containing measured snowfall data from the Minneapolis St. Paul Area:
+#'
+#' \itemize{
+#'   \item date. Calendar day of year of measurement
+#'   \item time. Time measurement is made
+#'   \item station_id Shorthand name for station in CoCoRaHS network
+#'   \item station_name Name of station in CoCoRaHS network
+#'   \item snowfall total snowfall (inches)
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name snowfall
+#' @usage data(snowfall)
+#' @format A data frame with 16 rows and 5 variables
+#' @source The data were collected from Community Collaborative Rain Hail and Snow Network (CoCoRaHS). \url{https://www.cocorahs.org/ViewData/ListDailyPrecipReports.aspx}
+NULL
