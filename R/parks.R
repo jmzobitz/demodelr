6030cb104a8c9d390a12f68d6591afa462f7f388
jmzobitz <zobitz@augsburg.edu>
2018-10-09 15:23:06 -0500

    parks + new data

---
+++ R/parks.R
@@ -0,0 +1,18 @@
+#' Visitor and resource usage to a national park
+#'
+#' A dataset containing phosophorous content in Daphnia and algae. The variables are as follows:
+#'
+#' \itemize{
+#'   \item time. (days)
+#'   \item vistors. number of visitors to a national parked, scaled by the equilibrium value.
+#'   \item resources. scaled area of the reserve not deforested.
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name parks
+#' @usage data(parks)
+#' @format A data frame with 8 rows and 3 variables
+#' @source Laura Sinay, Leon Sinay. 2006. “A Simple Mathematical Model for the Effects of the Growth of Tourism on Environment.”
+
+NULL
