7d5876a34a339d0c934348dca47e8ca5d616ca62
jmzobitz <zobitz@augsburg.edu>
2018-12-01 14:35:27 -0600

    method updated

--- R/eulerStochastic.R
+++ R/eulerStochastic.R
@@ -25,6 +25,8 @@
 eulerStochastic <- function(deltaT=1,timeSteps=1,initialCondition,FUN=deterministicDynamics,FUN_2=stochasticDynamics,parameters=parameters,nSimulations=1,sigma=1) {
 
 
+  # Compute the number of variables:
+  nVariables = length(initialCondition)
   # Make a list of things we are bringing back
   run_results <- vector("list", nSimulations)
   time = seq(from=0,by=deltaT,length.out=timeSteps)  # the output time vector
@@ -36,7 +38,7 @@ eulerStochastic <- function(deltaT=1,timeSteps=1,initialCondition,FUN=determinis
 
     for (i in 2:timeSteps) {
       oldP = newP
-      newP <-  unlist(deterministicDynamics(time[i],oldP,parameters))*deltaT+ unlist(stochasticDynamics(time[i],oldP,parameters))*sigma*sqrt(deltaT)*rnorm(1)+oldP   # Your differential equation goes here.
+      newP <-  unlist(deterministicDynamics(time[i],oldP,parameters))*deltaT+ unlist(stochasticDynamics(time[i],oldP,parameters))*sigma*sqrt(deltaT)*rnorm(nVariables)+oldP   # Your differential equation goes here.
 
       ## Add in a reality constraint that they can't be negative
       newP <- pmax(newP,0)
